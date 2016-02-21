03af88075ee2819a92f11a34cc3c1e27c758743c
akosel <aaron@deepfield.net>
2016-02-21 00:39:06 -0500

    Add semi-colons
    Update README.md
    
    More semi-colons

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Buffers or Strings
 ```javascript
 var xor = require('bitwise-xor');
 
-xor(new Buffer('00ff', 'hex'), new Buffer('3344', 'hex'))
+xor(new Buffer('00ff', 'hex'), new Buffer('3344', 'hex'));
 
 // returns <Buffer 33 bb>
 ```
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-module.exports = xor
+module.exports = xor;
 
 function xor(a, b) {
-  if (!Buffer.isBuffer(a)) a = new Buffer(a)
-  if (!Buffer.isBuffer(b)) b = new Buffer(b)
-  var res = []
+  if (!Buffer.isBuffer(a)) a = new Buffer(a);
+  if (!Buffer.isBuffer(b)) b = new Buffer(b);
+  var res = [];
   if (a.length > b.length) {
     for (var i = 0; i < b.length; i++) {
-      res.push(a[i] ^ b[i])
+      res.push(a[i] ^ b[i]);
     }
   } else {
     for (var i = 0; i < a.length; i++) {
-      res.push(a[i] ^ b[i])
+      res.push(a[i] ^ b[i]);
     }
   }
   return new Buffer(res);
